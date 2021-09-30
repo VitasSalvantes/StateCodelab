a09d76bf8e8272c7149ce8b7146e36caa40db3fc
VitasSalvantes <salvantesvitas@gmail.com>
2021-09-30 12:55:33 +0200

    Added icons for TextField

--- start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -17,12 +17,7 @@
 package com.codelabs.state.todo
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -119,7 +114,9 @@ private fun randomTint(): Float {
 
 // TodoInputTextField with hoisted state
 @Composable
-fun TodoInputTextField(text: String, onTextChange: (String) -> Unit, modifier: Modifier) {
+fun TodoInputTextField(
+    text: String, onTextChange: (String) -> Unit, modifier: Modifier
+) {
 
     TodoInputText(text = text, onTextChange = onTextChange, modifier = modifier)
 }
@@ -127,29 +124,38 @@ fun TodoInputTextField(text: String, onTextChange: (String) -> Unit, modifier: M
 @Composable
 fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
     val (text, setText) = remember { mutableStateOf("") }
-
-    // onItemComplete is an event will fire when an item is completed by the user
+    val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default) }
+    val iconsVisible = text.isNotBlank()
+    val submit = {
+        onItemComplete(TodoItem(text, icon))
+        setIcon(TodoIcon.Default)
+        setText("")
+    }
     Column {
         Row(
             Modifier
                 .padding(horizontal = 16.dp)
                 .padding(top = 16.dp)
         ) {
-            TodoInputTextField(
-                text, setText,
-                Modifier
+            TodoInputText(
+                text = text,
+                onTextChange = setText,
+                modifier = Modifier
                     .weight(1f)
-                    .padding(end = 8.dp)
+                    .padding(end = 8.dp),
+                onImeAction = submit // pass the submit callback to TodoInputText
             )
-            // Edit TodoItemInput
             TodoEditButton(
-                onClick = {
-                    onItemComplete(TodoItem(text))  // send onItemComplete event up
-                    setText("")                     // clear the internal text
-                },
+                onClick = submit, // pass the submit callback to TodoEditButton
                 text = "Add",
-                modifier = Modifier.align(Alignment.CenterVertically)
+                modifier = Modifier.align(Alignment.CenterVertically),
+                enabled = text.isNotBlank()
             )
+        }
+        if (iconsVisible) {
+            AnimatedIconRow(icon, setIcon, Modifier.padding(top = 8.dp))
+        } else {
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
